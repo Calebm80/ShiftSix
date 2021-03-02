7b94213d1d5f517d5e339de0021a0302d6010b08
Caleb Hamilton <calebham2@yahoo.com>
2021-03-02 15:15:54 -0600

    Added module
    added modules and code related

---
+++ Modules/ExpenseModule.java
@@ -0,0 +1,41 @@
+// add an expense
+    //add note to expense
+    //list of basic expenditure
+        //food
+        //bills
+        //medical
+        //personal
+        //person to person transfer
+
+
+
+
+
+// remove expense
+    // shows a list of added expenses to remove one
+
+
+// reset lists
+
+
+
+// show top/bottom/etc expense
+
+
+
+// expected monthly expenditure
+
+
+// add budget (compare with expected)
+
+import java.io.*;
+import java.util.*;
+
+public class ExpenseModule {
+    public static void main(String[] args) {
+
+      //  ArrayList<String> cars = new ArrayList<String>();
+
+    }
+
+}
---
+++ Modules/Lists.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Lists extends ListIntteraction{
+
+    // sorted by recent list
+    ArrayList<String> myNotes = new ArrayList<String>();
+    ArrayList<Double> myNumbers = new ArrayList<Double>();
+
+    // sorted by expense list
+    ArrayList<String> organizeNotes = new ArrayList<String>();
+    ArrayList<Double> organizeNumbers = new ArrayList<Double>();
+
+
+
+
+}
