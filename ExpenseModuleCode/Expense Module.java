7f974559d9c3be5ae0ac6f96a99df246874915d7
Caleb Hamilton <calebham2@yahoo.com>
2021-03-02 16:23:51 -0600

    Figuring it out

---
+++ ExpenseModuleCode/Expense Module.java
@@ -0,0 +1,38 @@
+// add an expense
+    //add note to expense
+    //list of basic expenditure
+        //food
+        //bills
+        //medical
+        //personal
+        //person to person transfer
+
+
+
+
+
+// remove expense
+    // shows a list of added expenses to remove one
+
+
+// reset lists
+
+
+
+// show top/bottom/etc expense
+
+
+
+// expected monthly expenditure
+
+
+// add budget (compare with expected)
+
+
+public class ExpenseModule {
+    public static void main(String[] args) {
+
+
+    }
+
+}
