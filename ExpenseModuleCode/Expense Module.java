e87bcceef496321c887a017b8a38da73df6088e8
Caleb Hamilton <60898984+CalebHam@users.noreply.github.com>
2021-03-21 16:06:17 -0500

    Update Expense Module.java

--- ExpenseModuleCode/Expense Module.java
+++ ExpenseModuleCode/Expense Module.java
@@ -32,6 +32,14 @@
 public class ExpenseModule {
     public static void main(String[] args) {
 
+            Lists theList;
+            theList.initializeList();
+            theList.changeBudget(350.50);
+            print(theList.budget);
+            theList.addExpense(theList,23.20,"Tissues");
+            theList.addExpense(theList,44.30,"Tuxedo");
+            theList.addExpense(theList,1.50,"Gum");
+            theList.organizeExpense();
 
     }
 
