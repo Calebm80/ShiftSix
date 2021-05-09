191246aa9c8a134321221ef6ff0c5405181488dd
Blub <mr.millian@gmail.com>
2021-05-09 17:27:34 -0500

    expense fragment baseline

--- ShiftSix/app/src/main/java/com/example/shiftsix/ExpenseFragment.java
+++ ShiftSix/app/src/main/java/com/example/shiftsix/ExpenseFragment.java
@@ -11,65 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 
+import com.example.shiftsix.databinding.FragmentExpenseBinding;
 import com.example.shiftsix.databinding.FragmentSettingsBinding;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class ExpenseFragment extends Fragment {
-//    FragmentSettingsBinding binding;
-//    SharedPreferences sharedPreferences;
+    FragmentExpenseBinding binding;
 
-    public ExpenseFragment(SharedPreferences sharedPreferences) {
-        this.sharedPreferences = sharedPreferences;
+    public ExpenseFragment() {
+        // required empty constructor
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentSettingsBinding.inflate(getLayoutInflater());
-        initMenuSettings();
+        binding = FragmentExpenseBinding.inflate(getLayoutInflater());
 
         return binding.getRoot();
     }
-//
-//    private void initMenuSettings() {
-//        initSwitches();
-//    }
-//
-//    private void initSwitches() {
-//        SwitchMaterial switchMaterial = binding.calendarMenuVisibility;
-//        initSwitch(switchMaterial, "calendarMenuVisibility");
-//
-//        switchMaterial = binding.expenseMenuVisibility;
-//        initSwitch(switchMaterial, "expenseMenuVisibility");
-//    }
-//
-//    private void initSwitch(SwitchMaterial switchMaterial, String key) {
-//        switchMaterial.setChecked(sharedPreferences.getBoolean(key, true));
-//
-//        switchMaterial.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-//                    System.out.println(buttonView.getId() + " CHECKED");
-//                    updatePreference(key, true);
-//                } else {
-//                    System.out.println(buttonView.getId() + " UNCHECKED");
-//                    updatePreference(key, false);
-//                }
-//            }
-//        });
-//    }
-//
-//    private void updatePreference(String key, int value) {
-//        SharedPreferences.Editor editor = sharedPreferences.edit();
-//        editor.putInt(key, value);
-//        editor.apply(); // ASYNCHRONOUS - USE commit() FOR SYNCHRONOUS
-//    }
-//
-//    private void updatePreference(String key, boolean value) {
-//        SharedPreferences.Editor editor = sharedPreferences.edit();
-//        editor.putBoolean(key, value);
-//        editor.apply(); // ASYNCHRONOUS - USE commit() FOR SYNCHRONOUS
-//    }
 }
